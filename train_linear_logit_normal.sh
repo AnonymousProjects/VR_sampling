4150688f490bd3a186baafd731c41093c291ae14
AnonymousProjects <142711886+AnonymousProjects@users.noreply.github.com>
2024-10-07 12:00:33 +0800

    Add files via upload

---
+++ train_linear_logit_normal.sh
@@ -0,0 +1,3 @@
+mean="0.75"
+var="1"
+torchrun --nnodes=1 --nproc_per_node=8 train.py --model SiT-XL/2 --data-path /mnt/data/public/dataset/imagenet/ILSVRC/Data/CLS-LOC/train --num-classes 1000 --logit_normal True --ln_mu $mean --ln_sigma $var
